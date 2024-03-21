5d60ba827c8fb98b2a3ba3894e4298ef7f0d43e4
skippyall <>
2024-03-21 22:15:35 +0100

    .

--- server/src/main/java/io/github/skippyall/vote/server/Server.java
+++ server/src/main/java/io/github/skippyall/vote/server/Server.java
@@ -1,6 +1,8 @@
 package io.github.skippyall.vote.server;
 
 import io.github.skippyall.vote.core.VoteCore;
+import io.github.skippyall.vote.core.config.Config;
+import io.github.skippyall.vote.core.network.NetworkManager;
 
 import java.util.Scanner;
 
@@ -8,6 +10,7 @@ public class Server {
     public static void main(String[] args) {
         ServerEnvironment.init();
         VoteCore.init();
+        NetworkManager.startServer(Config.getPort());
         Scanner in = new Scanner(System.in);
         while(true){
             if (in.next().equals("stop")) {
