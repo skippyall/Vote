69e113f65dd78865bbd2f841b20e6d4d0d1c9745
skippyall <121978267+skippyall@users.noreply.github.com>
2024-03-22 10:00:43 +0100

    Update CommentStorage.java

--- core/src/main/java/io/github/skippyall/vote/core/comment/CommentStorage.java
+++ core/src/main/java/io/github/skippyall/vote/core/comment/CommentStorage.java
@@ -1,1 +1,9 @@
+package io.github.skippyall.vote.core.comment;
 
+public interface CommentStorage extends Storage {
+  Set<Comment> getComments(Vote vote);
+  Comment getComment(long id);
+  void addComment(Vote vote, Comment comment);
+  void removeComment(Comment comment);
+  void removeComment(long id);
+}
