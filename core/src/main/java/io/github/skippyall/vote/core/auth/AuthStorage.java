1ee704d8ecdd6dd6f9d6b849552e80e689d60753
skippyall <121978267+skippyall@users.noreply.github.com>
2024-03-22 10:08:46 +0100

    Update AuthStorage.java

--- core/src/main/java/io/github/skippyall/vote/core/auth/AuthStorage.java
+++ core/src/main/java/io/github/skippyall/vote/core/auth/AuthStorage.java
@@ -1,8 +1,24 @@
 package io.github.skippyall.vote.core.auth;
 
 public interface AuthStorage extends Storage {
-  User getUser(AuthID id);
-  Set<AuthID> getAuthIds(User user, String authType);
-  void addAuthId(User user, AuthID id);
-  void removeAuthId(User user, AuthID id);
+  long getUserId(AuthID id);
+  Set<AuthID> getAuthIds(long user, String authType);
+  void addAuthId(long user, AuthID id);
+  void removeAuthId(long user, AuthID id);
+
+  default User getUser(AuthID id){
+    return Storages.USER_STORAGE.getUser(getUserId(id));
+  }
+
+  default Set<AuthID> getAuthIds(User user, String authType){
+    return getAuthIds(user.getId(), authType);
+  }
+
+  default void addAuthId(User user, AuthID id){
+    return addAuthId(user.getId(), id);
+  }
+
+  default void addAuthId(User user, AuthID id){
+    return removeAuthId(user.getId(), id);
+  }
 }
