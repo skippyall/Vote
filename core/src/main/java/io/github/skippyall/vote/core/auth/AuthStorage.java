66fa8279d849096bd7a881f24e934579dec13b50
skippyall <121978267+skippyall@users.noreply.github.com>
2024-03-22 09:51:47 +0100

    Update AuthStorage.java

--- core/src/main/java/io/github/skippyall/vote/core/auth/AuthStorage.java
+++ core/src/main/java/io/github/skippyall/vote/core/auth/AuthStorage.java
@@ -1,1 +1,8 @@
+package io.github.skippyall.vote.core.auth;
 
+public interface AuthStorage extends Storage {
+  User getUser(AuthID id);
+  Set<AuthID> getAuthIds(User user, String authType);
+  void addAuthId(User user, AuthID id);
+  void removeAuthId(User user, AuthID id);
+}
